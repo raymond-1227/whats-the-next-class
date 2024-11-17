1f7d00754804fb1b80cf267c2659b77e78c56046
Raymond Hsu <raymondhsu@duck.com>
2024-11-18 01:56:19 +0800

    fix sw
    service workers omg

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,16 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js");
+
+const CACHE = "pwabuilder-offline";
+
+self.addEventListener("message", (event) => {
+    if (event.data && event.data.type === "SKIP_WAITING") {
+      self.skipWaiting();
+    }
+  });
+  
+  workbox.routing.registerRoute(
+    new RegExp('/*'),
+    new workbox.strategies.StaleWhileRevalidate({
+      cacheName: CACHE
+    })
+  );
